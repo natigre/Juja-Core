9119ee91387f72baccd982d872cb6d2551e1286c
Natalia <mazurnata84@gmail.com>
2017-06-27 14:10:35 +0300

    some changes in Lab07

--- juja-core/src/ua/com/juja/core/labs/BitUtils.java
+++ juja-core/src/ua/com/juja/core/labs/BitUtils.java
@@ -6,7 +6,7 @@ Lab07
 public class BitUtils {
     public static byte swapBits(byte b, int i, int j) {
         if(((b >> i) & 1) != ((b >> j) & 1)) {
-            b ^= (1L << i) | (1L << j);
+            b ^= (1 << i) | (1 << j);
         }
         return b;
     }
