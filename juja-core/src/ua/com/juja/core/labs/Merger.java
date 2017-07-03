f463e13a6a8bc69fe8c9fb02292d0033a84a7b0a
natigre <mazurnata84@gmail.com>
2017-07-03 13:25:31 +0300

    lab10

---
+++ juja-core/src/ua/com/juja/core/labs/Merger.java
@@ -0,0 +1,37 @@
+package ua.com.juja.core.labs;
+
+import java.util.Arrays;
+/*
+lab10
+ */
+
+public class Merger {
+    public static int[] merge(int[] fst, int[] snd) {
+        int[] result = new int[fst.length + snd.length];
+        int fstIndex = 0;
+        int sndIndex = 0;
+
+        while ( fstIndex < fst.length && sndIndex < snd.length ) {
+            if ( fst[fstIndex] < snd[sndIndex] ) {
+                result[fstIndex + sndIndex] = fst[fstIndex++];
+            } else {
+                result[fstIndex + sndIndex] = snd[sndIndex++];
+            }
+        }
+        while ( fstIndex < fst.length ) {
+            result[fstIndex + sndIndex] = fst[fstIndex++];
+        }
+        while ( sndIndex < snd.length ) {
+            result[fstIndex + sndIndex] = snd[sndIndex++];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] fst = {1, 2, 3, 4, 5, 16};
+        int[] snd = {6, 7, 8, 9, 10, 11, 12};
+
+        int[] result = merge(fst, snd);
+        System.out.println(Arrays.toString(result));
+    }
+}
