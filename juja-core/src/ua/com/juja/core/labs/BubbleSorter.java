1dc89548bc7560cc4ae9ad477e17d91d996d5d80
Natalia <mazurnata84@gmail.com>
2017-08-31 21:47:06 +0300

    lab14

---
+++ juja-core/src/ua/com/juja/core/labs/BubbleSorter.java
@@ -0,0 +1,29 @@
+package ua.com.juja.core.labs;
+/*
+Lab14
+ for (int barrier = arr.length - 1; barrier >= 0; barrier--) {
+            for (int index = 0; index < barrier; index++) {
+                if (arr[index] > arr[index + 1]) {
+                    int tmp = arr[index];
+                    arr[index] = arr[index + 1];
+                    arr[index + 1] = tmp;
+                }
+            }
+        }
+ */
+
+public class BubbleSorter {
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ juja-core/src/ua/com/juja/core/labs/TestToLab14.java
@@ -0,0 +1,22 @@
+package ua.com.juja.core.labs;
+
+import java.util.Arrays;
+
+public class TestToLab14 {
+    public static void main(String[] args) {
+        // fill elements
+        int[] array = new int[] {2, 1};
+        // library sort
+        int[] expected = array.clone();
+        Arrays.sort(expected);
+        // lab sort
+        int[] actual = array.clone();
+        BubbleSorter.sort(actual);
+        // compare results: library vs lab
+        if (!Arrays.equals(actual, expected)) {
+            throw new AssertionError("expected = " + Arrays.toString(expected) + " but actual  = " + Arrays.toString(actual));
+        }
+
+        System.out.print("OK");
+    }
+}
