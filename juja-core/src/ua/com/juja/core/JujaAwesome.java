b686b65a7275809a0edf233fd200793d02278856
Natalia <mazurnata84@gmail.com>
2017-09-24 12:35:49 +0300

    add gitignore and past lab

---
+++ juja-core/src/ua/com/juja/core/JujaAwesome.java
@@ -0,0 +1,13 @@
+package ua.com.juja.core;
+
+public class JujaAwesome {
+
+    public static void printJujaIsAwesome() {
+        System.out.println("Juja is awesome!"); /*BODY*/
+    }
+    public static void main(String[] args) {
+        printJujaIsAwesome();
+    }
+}
+
+
